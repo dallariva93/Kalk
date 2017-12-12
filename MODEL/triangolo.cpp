665ef46919b2bb31bb8e7b99af58de8b2f329d05
FedericaRamina <federica.ramina94@gmail.com>
2017-12-12 12:50:42 +0100

    implementato triangoloo ancoraaa

--- MODEL/triangolo.cpp
+++ MODEL/triangolo.cpp
@@ -4,6 +4,7 @@
 
 
 triangolo::triangolo(double latoA, double latoB, angolo c, colore* col) : poligono(3, "triangolo", col) {
+//angolo compreso tra  i due lati
     QVector<punto> punti;
     punti.push_back(punto(0,0));
     punti.push_back(punto(latoA,0));
@@ -15,7 +16,7 @@ triangolo::triangolo(double latoA, double latoB, angolo c, colore* col) : poligo
 
 triangolo::triangolo(double latoA, angolo b, angolo c, colore* col) : poligono(3, "triangolo", col) {
     angolo a = 180-b.getAngolo()-c.getAngolo();
-    double latoC = (latoA * sin(b.getAngolo())) / sin(a.getAngolo());
+    double latoC = (latoA * sin(c.getAngolo())) / sin(a.getAngolo());
     QVector<punto> punti;
     punti.push_back(punto(0,0));
     punti.push_back(punto(latoA,0));
@@ -28,19 +29,19 @@ triangolo::triangolo(double latoA, angolo b, angolo c, colore* col) : poligono(3
 //    double latoC=( latoA * sin(c) ) / sin(a);
 }
 
-triangolo::triangolo(double latoA, double latoB, double latoC, colore* col){
+triangolo::triangolo(double latoA, double latoB, double latoC, colore* col) : poligono(3, "triangolo", col) {
     double cos_a = ( pow(latoA,2) + pow(latoB,2) + pow(latoC,2) ) / 2*latoB*latoC ;
-
+    angolo a = acos(cos_a);
+    QVector<punto> punti;
+    punti.push_back(punto(0,0));
+    punti.push_back(punto(latoB,0));
+    double x= latoC * cos(a.getAngolo());
+    double y= latoC * sin(a.getAngolo());
+    punti.push_back(punto(x,y));
+    setPunti(punti);
 }
 /*
 double triangolo::getArea()const{
 
 }*/
-double triangolo::getPerimetro(){
-    QVector<punto> coord=this->getCoordinate();
-    return  punto::distanceTo(coord[0],coord[1])+
-            punto::distanceTo(coord[1],coord[2])+
-            punto::distanceTo(coord[0],coord[2]);
-}
-
 
